90866c1bb18943d0c2ad057dc61fec4654324a95
Stefan Danescu <stefi_d27@yahoo.com>
2020-06-29 19:33:03 +0300

    user added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const registerRouter = require('./routers/register')
 const loginRouter = require('./routers/login')
 const jobRouter = require('./routers/job')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(registerRouter)
 app.use(loginRouter)
 app.use(jobRouter)
+app.use(userRouter)
 
 app.get("/", (req, res) => {
     res.send("Router page")
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const User = require('../models/user')
+const { ObjectID } = require('mongodb')
 const router = new express.Router()
 
+router.get('/user/:id', async (req, res) => {
+    id = new ObjectID(req.params.id)
 
+    try {
+
+        const user = await User.findById(id)
+        return res.send(user.name)
+
+    } catch(e) {
+
+        if (!user) {
+            return res.status(404).send('Userul nu a putut fi gasit!')
+        }
+
+        return res.status(400).send(e.message)
+
+    }
+
+})
 
 module.exports = router
